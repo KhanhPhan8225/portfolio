d4c37090dd4a95ea98ca00c8b4fe9d52ef0e632b
Khánh Phan <64197816+KhanhPhan8225@users.noreply.github.com>
2021-10-22 17:05:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function getImage(){
-  const URL = 'https://api.giphy.com/v1/gifs/search'
+  const URL = 'https://api.giphy.com/v1/gifs/random'
   const API_KEY = 'Nv6mvO4VUU9qils4LvpVKjgsNcgaCIqx'
   
   const FULL_URL = URL + '?api_key' + API_KEY + '&tag=happybirthday'
