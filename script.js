660f0d1dc97fa9fd2f25dfd7313c5b87484a309e
Khánh Phan <64197816+KhanhPhan8225@users.noreply.github.com>
2021-10-22 16:43:43 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+async function getImage(){
+  const URL = 'https://api.giphy.com/v1/gifs/search'
+  const API_KEY = 'Nv6mvO4VUU9qils4LvpVKjgsNcgaCIqx'
+  
+  const FULL_URL = URL + '?api_key' + API_KEY + '&tag=happybirthday'
+  
+  const result = await fetch(FULL_URL).then(res =>res.json())
+  return result.data.image_url
+}
+
+async fuction run() {
+  const image = await getImage()
+  
+  const img = document.querySelector("#sn")
+  div.setAttribute('src', image)
+  
+  run()
